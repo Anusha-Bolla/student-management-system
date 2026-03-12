81a9b4729dad9f3223631495cd90e9719b541d70
Anusha-Bolla <anushae3773@gmail.com>
2026-03-12 19:19:14 -0400

    Add REST endpoint for students

--- backend/src/main/java/com/anusha/student/controller/StudentController.java
+++ backend/src/main/java/com/anusha/student/controller/StudentController.java
@@ -1,7 +1,12 @@
 package com.anusha.student.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
 
+    @GetMapping("/students")
     public String getStudents() {
         return "List of students";
     }
